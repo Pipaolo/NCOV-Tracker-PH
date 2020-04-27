6824c850d83b9c2d0eb70040bb9e273d38c0b732
Paolo Tolentino <tolentinopaolomatthew23@gmail.com>
2020-04-27 10:11:33 +0800

    Added Attributions to icons

--- lib/ui/info_page/info_page.dart
+++ lib/ui/info_page/info_page.dart
@@ -24,184 +24,250 @@ class InfoPage extends StatelessWidget {
               borderRadius: BorderRadius.circular(20),
             ),
             padding: const EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                SelectableText.rich(
-                  TextSpan(
-                    text: 'Made By: ',
-                    style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.bold,
-                      fontSize: ScreenUtil().setSp(40),
-                      color: Colors.white,
-                    ),
-                    children: [
-                      TextSpan(
-                        text: '\nPaolo Matthew Tolentino',
-                        style: GoogleFonts.montserrat(
-                          fontSize: ScreenUtil().setSp(25),
-                        ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  SelectableText.rich(
+                    TextSpan(
+                      text: 'Made By: ',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold,
+                        fontSize: ScreenUtil().setSp(40),
+                        color: Colors.white,
                       ),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SelectableText.rich(
-                  TextSpan(
-                    text: 'Contact: ',
-                    style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.bold,
-                      fontSize: ScreenUtil().setSp(40),
-                      color: Colors.white,
-                    ),
-                    children: [
-                      TextSpan(
-                        text: '\ntolentinopaolomatthew23@gmail.com',
-                        style: GoogleFonts.montserrat(
-                          fontSize: ScreenUtil().setSp(25),
-                        ),
-                      ),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SelectableText.rich(
-                  TextSpan(
-                    text: 'About The App ',
-                    style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.bold,
-                      fontSize: ScreenUtil().setSp(40),
-                      color: Colors.white,
-                    ),
-                    children: [
-                      TextSpan(
-                        text:
-                            '\nThe app was built with a purpose of providing realtime updates about the cases of the Coronavirus in the Philippines.',
-                        style: GoogleFonts.montserrat(
-                          fontSize: ScreenUtil().setSp(25),
-                        ),
-                      ),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SelectableText.rich(
-                  TextSpan(
-                    text: 'Disclaimer:',
-                    style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.bold,
-                      fontSize: ScreenUtil().setSp(40),
-                      color: Colors.white,
-                    ),
-                    children: [
-                      TextSpan(
-                        text:
-                            '\nAll of the data that is being shown here is not mine nor do I claim it. See references below.',
-                        style: GoogleFonts.montserrat(
-                          fontSize: ScreenUtil().setSp(25),
-                        ),
-                      ),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SelectableText.rich(
-                  TextSpan(
-                    text: 'Source Code:',
-                    style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.bold,
-                      fontSize: ScreenUtil().setSp(40),
-                      color: Colors.white,
-                    ),
-                    children: [
-                      TextSpan(
-                        text: '\nhttps://github.com/Pipaolo/NCOV-Tracker-PH',
-                        style: GoogleFonts.montserrat(
-                          fontSize: ScreenUtil().setSp(25),
-                        ),
-                      ),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SelectableText.rich(
-                  TextSpan(
-                    text: 'Special Thanks to:',
-                    style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.bold,
-                      fontSize: ScreenUtil().setSp(40),
-                      color: Colors.white,
-                    ),
-                    children: [
-                      TextSpan(
-                        text:
-                            '\nHyubs for creating an awesome api for keeping track of the COVID 19 in the Philippines.',
-                        style: GoogleFonts.montserrat(
-                          fontSize: ScreenUtil().setSp(25),
-                        ),
-                      ),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SelectableText.rich(
-                  TextSpan(
-                    text: 'References:',
-                    style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.bold,
-                      fontSize: ScreenUtil().setSp(40),
-                      color: Colors.white,
-                    ),
-                    children: [
-                      TextSpan(
-                          text: '\nDepartment of Health: ',
+                      children: [
+                        TextSpan(
+                          text: '\nPaolo Matthew Tolentino',
                           style: GoogleFonts.montserrat(
                             fontSize: ScreenUtil().setSp(25),
                           ),
-                          children: [
-                            TextSpan(
-                              text: 'https://ncovtracker.doh.gov.ph/',
-                              style: GoogleFonts.montserrat(
-                                decoration: TextDecoration.underline,
-                              ),
-                            ),
-                          ]),
-                      TextSpan(
-                        text:
-                            '\n\nUniversity of The Philippines ENDCOV Website: ',
-                        style: GoogleFonts.montserrat(
-                          fontSize: ScreenUtil().setSp(25),
                         ),
-                        children: [
-                          TextSpan(
-                            text: 'https://endcov.ph',
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SelectableText.rich(
+                    TextSpan(
+                      text: 'Contact: ',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold,
+                        fontSize: ScreenUtil().setSp(40),
+                        color: Colors.white,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: '\ntolentinopaolomatthew23@gmail.com',
+                          style: GoogleFonts.montserrat(
+                            fontSize: ScreenUtil().setSp(25),
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  SelectableText.rich(
+                    TextSpan(
+                      text: 'About The App ',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold,
+                        fontSize: ScreenUtil().setSp(40),
+                        color: Colors.white,
+                      ),
+                      children: [
+                        TextSpan(
+                          text:
+                              '\nThe app was built with a purpose of providing realtime updates about the cases of the Coronavirus in the Philippines.',
+                          style: GoogleFonts.montserrat(
+                            fontSize: ScreenUtil().setSp(25),
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SelectableText.rich(
+                    TextSpan(
+                      text: 'Disclaimer:',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold,
+                        fontSize: ScreenUtil().setSp(40),
+                        color: Colors.white,
+                      ),
+                      children: [
+                        TextSpan(
+                          text:
+                              '\nAll of the data that is being shown here is not mine nor do I claim it. See references below.',
+                          style: GoogleFonts.montserrat(
+                            fontSize: ScreenUtil().setSp(25),
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SelectableText.rich(
+                    TextSpan(
+                      text: 'Source Code:',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold,
+                        fontSize: ScreenUtil().setSp(40),
+                        color: Colors.white,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: '\nhttps://github.com/Pipaolo/NCOV-Tracker-PH',
+                          style: GoogleFonts.montserrat(
+                            fontSize: ScreenUtil().setSp(25),
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SelectableText.rich(
+                    TextSpan(
+                      text: 'Special Thanks to:',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold,
+                        fontSize: ScreenUtil().setSp(40),
+                        color: Colors.white,
+                      ),
+                      children: [
+                        TextSpan(
+                          text:
+                              '\nHyubs for creating an awesome api for keeping track of the COVID 19 in the Philippines.',
+                          style: GoogleFonts.montserrat(
+                            fontSize: ScreenUtil().setSp(25),
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SelectableText.rich(
+                    TextSpan(
+                      text: 'References:',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold,
+                        fontSize: ScreenUtil().setSp(40),
+                        color: Colors.white,
+                      ),
+                      children: [
+                        TextSpan(
+                            text: '\nDepartment of Health: ',
                             style: GoogleFonts.montserrat(
-                              decoration: TextDecoration.underline,
+                              fontSize: ScreenUtil().setSp(25),
                             ),
-                          ),
-                        ],
-                      ),
-                      TextSpan(
-                          text: '\n\nGraphQL API for COVID-19 (Philippines): ',
+                            children: [
+                              TextSpan(
+                                text: 'https://ncovtracker.doh.gov.ph/',
+                                style: GoogleFonts.montserrat(
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ]),
+                        TextSpan(
+                          text:
+                              '\n\nUniversity of The Philippines ENDCOV Website: ',
                           style: GoogleFonts.montserrat(
                             fontSize: ScreenUtil().setSp(25),
                           ),
                           children: [
                             TextSpan(
-                              text: 'https://ncovph.com',
+                              text: 'https://endcov.ph',
                               style: GoogleFonts.montserrat(
                                 decoration: TextDecoration.underline,
                               ),
                             ),
-                          ]),
-                    ],
+                          ],
+                        ),
+                        TextSpan(
+                            text:
+                                '\n\nGraphQL API for COVID-19 (Philippines): ',
+                            style: GoogleFonts.montserrat(
+                              fontSize: ScreenUtil().setSp(25),
+                            ),
+                            children: [
+                              TextSpan(
+                                text: 'https://ncovph.com',
+                                style: GoogleFonts.montserrat(
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ]),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SelectableText.rich(
+                    TextSpan(
+                      text: 'Icons:',
+                      style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold,
+                        fontSize: ScreenUtil().setSp(40),
+                        color: Colors.white,
+                      ),
+                      children: [
+                        TextSpan(
+                          text:
+                              '\nSplash page icon made by wanicon from www.flaticon.com',
+                          style: GoogleFonts.montserrat(
+                            fontSize: ScreenUtil().setSp(25),
+                          ),
+                        ),
+                        TextSpan(
+                          text:
+                              '\n\nTotal Infected icon made by Freepik from www.flaticon.com',
+                          style: GoogleFonts.montserrat(
+                            fontSize: ScreenUtil().setSp(25),
+                          ),
+                        ),
+                        TextSpan(
+                          text:
+                              '\n\nTotal Deaths icon made by Freepik from www.flaticon.com',
+                          style: GoogleFonts.montserrat(
+                            fontSize: ScreenUtil().setSp(25),
+                          ),
+                        ),
+                        TextSpan(
+                          text:
+                              '\n\nTotal Tests Conducted icon made by Freepik from www.flaticon.com',
+                          style: GoogleFonts.montserrat(
+                            fontSize: ScreenUtil().setSp(25),
+                          ),
+                        ),
+                        TextSpan(
+                          text:
+                              '\n\nTotal Recovered icon made by Pixel perfect from www.flaticon.com',
+                          style: GoogleFonts.montserrat(
+                            fontSize: ScreenUtil().setSp(25),
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
