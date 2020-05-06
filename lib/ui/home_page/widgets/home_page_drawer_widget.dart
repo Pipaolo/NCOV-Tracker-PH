2fb01eeccace77106c51ae5b517b57676e1893e3
Paolo Tolentino <tolentinopaolomatthew23@gmail.com>
2020-05-06 15:45:46 +0800

    Fixed app drawer

--- lib/ui/home_page/widgets/home_page_drawer_widget.dart
+++ lib/ui/home_page/widgets/home_page_drawer_widget.dart
@@ -30,17 +30,18 @@ class HomePageDrawerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.blue, Colors.blue.withOpacity(0.5)],
-                begin: Alignment.bottomCenter,
-                end: Alignment.topCenter,
-              ),
-            ),
-            child: DrawerHeader(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Colors.blue, Colors.blue.withOpacity(0.5)],
+            stops: [0.6, 0.65, 1],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+        ),
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
               child: Center(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -61,79 +62,89 @@ class HomePageDrawerWidget extends StatelessWidget {
                 ),
               ),
             ),
-          ),
-          ListTile(
-            leading: FaIcon(
-              FontAwesomeIcons.userInjured,
-              color: Colors.blueAccent,
+            Container(
+              color: Colors.white,
+              child: ListView(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                children: <Widget>[
+                  ListTile(
+                    leading: FaIcon(
+                      FontAwesomeIcons.userInjured,
+                      color: Colors.blueAccent,
+                    ),
+                    trailing: FaIcon(FontAwesomeIcons.angleRight),
+                    title: Text('Cases'),
+                    onTap: () {
+                      context.bloc<CasesPageBloc>()..add(CasesFetched());
+                      ExtendedNavigator.of(context).pop();
+                      ExtendedNavigator.of(context)
+                          .pushNamed(Routes.casesPageRoute);
+                      // Flushbar(
+                      //   title: '🛠👷‍♀️ Under Development 👷‍♂️🛠',
+                      //   message: 'This feature is coming soon!',
+                      //   margin: const EdgeInsets.all(10),
+                      //   borderRadius: 10,
+                      //   reverseAnimationCurve: Curves.easeOut,
+                      //   duration: Duration(seconds: 2),
+                      // )..show(context);
+                    },
+                  ),
+                  ListTile(
+                    leading: FaIcon(
+                      FontAwesomeIcons.hospital,
+                      color: Colors.blueAccent,
+                    ),
+                    trailing: FaIcon(FontAwesomeIcons.angleRight),
+                    title: Text('Hospital Directories'),
+                    onTap: () {
+                      ExtendedNavigator.of(context).pop();
+                      BlocProvider.of<HospitalBloc>(context)
+                        ..add(HospitalListingsPressed());
+                      ExtendedNavigator.of(context)
+                          .pushNamed(Routes.hospitalListingsPageRoute);
+                    },
+                  ),
+                  ListTile(
+                    leading: FaIcon(
+                      FontAwesomeIcons.infoCircle,
+                      color: Colors.blueAccent,
+                    ),
+                    trailing: FaIcon(FontAwesomeIcons.angleRight),
+                    title: Text('About The App'),
+                    onTap: () {
+                      ExtendedNavigator.of(context).pop();
+                      ExtendedNavigator.of(context).pushInfoPageRoute();
+                    },
+                  ),
+                  ListTile(
+                    leading: FaIcon(
+                      FontAwesomeIcons.phoneAlt,
+                      color: Colors.blueAccent,
+                    ),
+                    trailing: FaIcon(FontAwesomeIcons.angleRight),
+                    title: Text('DOH Emergency Hotline'),
+                    onTap: () {
+                      ExtendedNavigator.of(context).pop();
+                      _showDOHDialog(context);
+                    },
+                  ),
+                  ListTile(
+                    leading: FaIcon(
+                      FontAwesomeIcons.mapMarkedAlt,
+                      color: Colors.blueAccent,
+                    ),
+                    trailing: FaIcon(FontAwesomeIcons.angleRight),
+                    title: Text('NCOV Heat Map'),
+                    onTap: () {
+                      ExtendedNavigator.of(context).pushMapPageRoute();
+                    },
+                  ),
+                ],
+              ),
             ),
-            trailing: FaIcon(FontAwesomeIcons.angleRight),
-            title: Text('Cases'),
-            onTap: () {
-              context.bloc<CasesPageBloc>()..add(CasesFetched());
-              ExtendedNavigator.of(context).pop();
-              ExtendedNavigator.of(context).pushNamed(Routes.casesPageRoute);
-              // Flushbar(
-              //   title: '🛠👷‍♀️ Under Development 👷‍♂️🛠',
-              //   message: 'This feature is coming soon!',
-              //   margin: const EdgeInsets.all(10),
-              //   borderRadius: 10,
-              //   reverseAnimationCurve: Curves.easeOut,
-              //   duration: Duration(seconds: 2),
-              // )..show(context);
-            },
-          ),
-          ListTile(
-            leading: FaIcon(
-              FontAwesomeIcons.hospital,
-              color: Colors.blueAccent,
-            ),
-            trailing: FaIcon(FontAwesomeIcons.angleRight),
-            title: Text('Hospital Directories'),
-            onTap: () {
-              ExtendedNavigator.of(context).pop();
-              BlocProvider.of<HospitalBloc>(context)
-                ..add(HospitalListingsPressed());
-              ExtendedNavigator.of(context)
-                  .pushNamed(Routes.hospitalListingsPageRoute);
-            },
-          ),
-          ListTile(
-            leading: FaIcon(
-              FontAwesomeIcons.infoCircle,
-              color: Colors.blueAccent,
-            ),
-            trailing: FaIcon(FontAwesomeIcons.angleRight),
-            title: Text('About The App'),
-            onTap: () {
-              ExtendedNavigator.of(context).pop();
-              ExtendedNavigator.of(context).pushInfoPageRoute();
-            },
-          ),
-          ListTile(
-            leading: FaIcon(
-              FontAwesomeIcons.phoneAlt,
-              color: Colors.blueAccent,
-            ),
-            trailing: FaIcon(FontAwesomeIcons.angleRight),
-            title: Text('DOH Emergency Hotline'),
-            onTap: () {
-              ExtendedNavigator.of(context).pop();
-              _showDOHDialog(context);
-            },
-          ),
-          ListTile(
-            leading: FaIcon(
-              FontAwesomeIcons.mapMarkedAlt,
-              color: Colors.blueAccent,
-            ),
-            trailing: FaIcon(FontAwesomeIcons.angleRight),
-            title: Text('NCOV Heat Map'),
-            onTap: () {
-              ExtendedNavigator.of(context).pushMapPageRoute();
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
